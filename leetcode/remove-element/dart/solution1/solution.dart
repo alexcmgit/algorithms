4da3ae7de9fb9f79be3d76aa32e5d646713e5b2b
Alex Castro <git@alexcastro.dev>
2025-10-06 12:38:51 -0300

    Create solution.dart

---
+++ leetcode/remove-element/dart/solution1/solution.dart
@@ -0,0 +1,10 @@
+class Solution {
+  int removeElement(List<int> nums, int val) {
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == val) {
+        nums.removeAt(i--);
+      }
+    }
+    return nums.length;
+  }
+}
