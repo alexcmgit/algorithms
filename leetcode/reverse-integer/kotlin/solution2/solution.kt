082fd7530a95a4aff86529e8bc6fd2a71ff0a0fd
Alex Castro <git@alexcastro.dev>
2025-10-07 01:19:32 -0300

    Update solution.kt

--- leetcode/reverse-integer/kotlin/solution2/solution.kt
+++ leetcode/reverse-integer/kotlin/solution2/solution.kt
@@ -1,9 +1,4 @@
 class Solution {
-    companion object {
-        const val min: Int = -2147483648
-        const val max: Int = 2147483647
-    }
-
     fun reverse(x: Int): Int {
         val t = if (x < 0) x * -1 else x
         val p = if (x < 0) "-" else ""
