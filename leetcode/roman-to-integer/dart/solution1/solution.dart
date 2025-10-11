c2aec095f045cb54459814dfa70b6c561a83ee69
Alex Castro <git@alexcastro.dev>
2025-10-11 14:01:00 -0300

    Update solution.dart

--- leetcode/roman-to-integer/dart/solution1/solution.dart
+++ leetcode/roman-to-integer/dart/solution1/solution.dart
@@ -4,29 +4,29 @@ class Solution {
     int v = 0;
 
     const m = <String?, int>{
-        null: 0,
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
+      null: 0,
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000,
     };
 
     for (int i = 0; i < s.length; i++) {
-        String c = s.charAt(i + 0);
-        String? n = s.charAtOrNull(i + 1);
+      String c = s.charAt(i + 0);
+      String? n = s.charAtOrNull(i + 1);
 
-        v = v + m[c]!;
+      v = v + m[c]!;
 
-        if (m[n]! > m[c]!) {
-            v = m[n]! - v;
-            i = i + 1;
-        }
+      if (m[n]! > m[c]!) {
+        v = m[n]! - v;
+        i = i + 1;
+      }
 
-        t = t + v;
-        v = 0;
+      t = t + v;
+      v = 0;
     }
 
     return t;
@@ -34,10 +34,11 @@ class Solution {
 }
 
 extension on String {
-    String? charAtOrNull(int index) {
-        return index >= length || index < 0 ? null : charAt(index);
-    }
-    String charAt(int index) {
-        return this[index];
-    }
+  String? charAtOrNull(int index) {
+    return index >= length || index < 0 ? null : charAt(index);
+  }
+
+  String charAt(int index) {
+    return this[index];
+  }
 }
